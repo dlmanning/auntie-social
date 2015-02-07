2cb369594a7edafcff5676cd69b1736d7acf7e85
David Manning <dlmanning@me.com>
2015-02-06 23:04:32 -0800

    Added sweet borders between tweets

--- js/tweet.jsx
+++ js/tweet.jsx
@@ -1,9 +1,13 @@
 var React = require('react');
 
+var styles = {
+	borderBottom: '1px solid black'
+};
+
 export class Tweet extends React.Component {
 	render () {
 		return (
-			<div>
+			<div style={styles}>
 				<h3>{this.props.user.name}</h3>
 				<p>{this.props.text}</p>
 			</div>
